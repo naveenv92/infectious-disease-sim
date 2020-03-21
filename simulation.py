51419d2b0fee25fc6b0d088224173e9a575627fc
Naveen Venkatesan <naveen.venkatesan@gmail.com>
2020-03-21 16:43:13 -0700

    Added comments to Particle class

--- simulation.py
+++ simulation.py
@@ -1,8 +1,19 @@
+# Graphical and numerical required libraries
 import matplotlib.pyplot as plt
 import numpy as np
 
+## Particle class -- keeps track of position, velocity, infection state, and number of other people infected
 class Particle:
 
+	## Initialize particle with location and velocity, and set default infected state to False
+	# _x -- x-position
+	# _y -- y-position
+	# _vx -- velocity in the x-direction
+	# _vy -- velocity in the y-direction
+	# _r -- size of point shown in simulation (only if showing simulation)
+	# _isInfected -- boolean representing if particle in infected
+	# _isRecovered -- boolean representing if particle has recovered (and therefore, cannot be infected again)
+	# _numberOfInfections -- number of other particles infected
 	def __init__(self, x=0, y=0, vx=1, vy=1, r=2):
 		self._x = x
 		self._y = y
@@ -11,37 +22,50 @@ class Particle:
 		self._r = r
 		self._isInfected = False
 		self._isRecovered = False
+		self._numberOfInfections = 0
 
+	## Returns the x-position of the particle
 	def x(self):
 		return self._x
 
+	## Sets the x-position of the particle
 	def set_x(self, x):
 		self._x = x
 
+	## Returns the y-position of the particle
 	def y(self):
 		return self._y
 
+	## Sets the y-position of the particle
 	def set_y(self, y):
 		self._y = y
 
+	## Returns the velocity component in the x-direction of the particle
 	def vx(self):
 		return self._vx
 
+	## Sets the velocity component in the x-direction of the particle
 	def set_vx(self, vx):
 		self._vx = vx
 
+	## Returns the velocity component in the y-direction of the particle
 	def vy(self):
 		return self._vy
 
+	## Sets the velocity component in the y-direction of the particle
 	def set_vy(self, vy):
 		self._vy = vy
 
+	## Returns the size of the point shown in the simulation
 	def r(self):
 		return self._r
 
+	## Sets the size of the point shown in the simulation
 	def set_r(self, r):
 		self._r = r
 
+	## Moves the particle by a timestep (default is 1) along its velocity vector and updates its position
+	# If infected, counts down timesteps until recovery
 	def move(self, dt=1):
 		self._x += self._vx*dt
 		self._y += self._vy*dt
@@ -52,16 +76,27 @@ class Particle:
 				self._isInfected = False
 				self._isRecovered = True
 
+	## Infects particle if it not yet been infected and recovered, and sets a number of timesteps until recovery
 	def infect(self):
 		if self._isRecovered == False and self._isInfected == False:
 			self._isInfected = True
-			self._infectedCounter = 20
+			self._infectedCounter = 30
 
+	## Returns the infection state of the particle (True or False)
 	def isInfected(self):
 		return self._isInfected
 
+	## Returns the recovered state of the particle (True or False)
 	def isRecovered(self):
 		return self._isRecovered
+
+	## Increment the number of other particles infected
+	def infectOtherPerson(self):
+		self._numberOfInfections += 1
+
+	## Returns the number of other particles infected
+	def numOfInfections(self):
+		return self._numberOfInfections
 
 
 class Simulation:
@@ -139,14 +174,20 @@ class Simulation:
 				particle_1 = self._particles[i]
 				particle_2 = self._particles[j]
 				if particle_1.x() - self._tol <= particle_2.x() <= particle_1.x() + self._tol and particle_1.y() - self._tol <= particle_2.y() <= particle_1.y() + self._tol:
-					if particle_1.isInfected() or particle_2.isInfected():
-						particle_1.infect()
+					if particle_1.isInfected() and not particle_2.isInfected() and not particle_2.isRecovered():
+						particle_1.infectOtherPerson()
 						particle_2.infect()
+					if particle_2.isInfected() and not particle_1.isInfected() and not particle_1.isRecovered():
+						particle_2.infectOtherPerson()
+						particle_1.infect()
+
 
 	def statistics(self):
 		countInf = 0
 		countRec = 0
 		countNI = 0
+		avgInfections = 0
+		numOfInfectors = 1
 		for i in self._particles:
 			if i.isInfected():
 				countInf += 1
@@ -154,8 +195,13 @@ class Simulation:
 				countRec += 1
 			else:
 				countNI += 1
+			if i.numOfInfections() > 0:
+				avgInfections += i.numOfInfections()
+				numOfInfectors += 1
 
-		return [countNI, countInf, countRec]
+		avgInfections = avgInfections/numOfInfectors
+
+		return [countNI, countInf, countRec, avgInfections]
 
 
 class runSimulation:
@@ -186,8 +232,8 @@ class runSimulation:
 			timesteps += 1
 			
 			if showSim:
-				plt.scatter(self._simulation.x_coords(), self._simulation.y_coords(), s=self._simulation.radii()[0]**2)
-				plt.scatter(self._simulation.x_coords_inf(), self._simulation.y_coords_inf(), s=self._simulation.radii()[0]**2)
+				plt.scatter(self._simulation.x_coords(), self._simulation.y_coords(), s=self._simulation.radii()[0]**2, color='red')
+				plt.scatter(self._simulation.x_coords_inf(), self._simulation.y_coords_inf(), s=self._simulation.radii()[0]**2, color='black')
 				plt.xticks([])
 				plt.yticks([])
 				plt.xlim(-0.01, self._simulation.getBoxSize() + 0.01)
@@ -204,6 +250,8 @@ class runSimulation:
 		self._numNI = numNI
 		self._numInf = numInf
 		self._numRec = numRec
+
+		print(self._simulation.statistics()[3])
 
 		if plot:
 			fig = plt.figure(figsize=(5,5))

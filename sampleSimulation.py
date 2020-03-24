6c71ccc12a2bd53bb3b117efd7aed28aea1c898f
Naveen Venkatesan <naveen.venkatesan@gmail.com>
2020-03-24 15:56:37 -0700

    Updating sampleSimulation.py

--- sampleSimulation.py
+++ sampleSimulation.py
@@ -1,6 +1,6 @@
 from simulation import *
 
-numberOfPeople = 376
-sim = Simulation(n=numberOfPeople, ninf=5, r=5, boxSize=2, speed=0.05, tol=0.02)
+numberOfParticles = 94*4
+sim = Simulation(n=numberOfParticles, ninf=1, r=5, boxSize=2, speed=0.05, tol=0.04)
 runSim = runSimulation(simulation=sim)
 runSim.singleSim()

fb81f6f0438e5e70026ddf72ad8507c4e14a4916
Naveen Venkatesan <naveen.venkatesan@gmail.com>
2020-04-14 19:02:38 -0700

    Removing Old Files

--- sampleSimulation.py
+++
@@ -1,6 +0,0 @@
-from simulation import *
-
-numberOfParticles = 94*4
-sim = Simulation(n=numberOfParticles, ninf=1, r=5, boxSize=2, speed=0.05, tol=0.04)
-runSim = runSimulation(simulation=sim)
-runSim.singleSim()

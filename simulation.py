26039c1b04fe0219994a31e5c35ec425f2b4de33
Naveen Venkatesan <naveen.venkatesan@gmail.com>
2020-04-16 17:00:44 -0700

    Updated simulation.py
    Created infection radius as input parameter to Simulation class and removed box size as parameter.

--- simulation.py
+++ simulation.py
@@ -16,6 +16,7 @@ class Particle:
         recovery_time (int): number of timesteps before recovery
         is_infected (bool): true when particle is infected
         is_recovered (bool): true when particle has recovered
+        infections (int): number of other particles infected
     """
 
     def __init__(self, x: float, y: float, vx: float, vy: float, recovery_time: int) -> None:
@@ -36,6 +37,7 @@ class Particle:
         self.infected_counter = -1
         self.is_infected = False
         self.is_recovered = False
+        self.infections = 0
 
     def __str__(self) -> str:
         """Return string of Particle object to print."""
@@ -63,12 +65,13 @@ class Particle:
             elif self.infected_counter > 0:
                 self.infected_counter -= 1
 
-    def infect(self) -> None:
+    def infect(self, other_particle: 'Particle') -> None:
         """Infects the particle if it has not already recovered."""
 
-        if not self.is_infected and not self.is_recovered:
-            self.is_infected = True
-            self.infected_counter = self.recovery_time
+        if not other_particle.is_infected and not other_particle.is_recovered:
+            self.infections += 1
+            other_particle.is_infected = True
+            other_particle.infected_counter = self.recovery_time
 
 
 class Simulation:
@@ -76,35 +79,34 @@ class Simulation:
     The Simulation object stores a collection of particles for simulation
 
     Attributes:
-        box_size (float): length of side of simulation box
         infections (int): number of initially infected particles
         particles (List[Particle]): list of particle objects in simulation
     """
 
-    def __init__(self, n: int, ninf: int, box_size: float, speed: float, recovery_time: int) -> None:
+    def __init__(self, n: int, ninf: int, rad: float, speed: float, recovery_time: int) -> None:
         """
         Parameters:
             n (int): number of particles in the simulation
             ninf (inf): number of initially infected particles
-            box_size (float): length of side of simulation box (area = box_size**2)
+            rad (float): infectious radius around particle
             speed (float): maximum speed of particles
             recovery_time (int): number of timesteps before recovery
         """
 
-        self.box_size = box_size
         self.infections = ninf
         self.particles = []
+        self.rad = rad
         # Healthy particles
         for _ in range(n - ninf):
-            x = box_size*np.random.random()
-            y = box_size*np.random.random()
+            x = np.random.random()
+            y = np.random.random()
             vx = speed*np.random.normal(scale=0.25)
             vy = speed*np.random.normal(scale=0.25)
             self.particles.append(Particle(x, y, vx, vy, recovery_time))
         # Infected particles
         for _ in range(ninf):
-            x = box_size*np.random.random()
-            y = box_size*np.random.random()
+            x = np.random.random()
+            y = np.random.random()
             vx = speed*np.random.normal(scale=0.25)
             vy = speed*np.random.normal(scale=0.25)
             particle = Particle(x, y, vx, vy, recovery_time)
@@ -131,22 +133,22 @@ class Simulation:
         if particle.x < 0:
             particle.x = 0
             particle.vx *= -1
-        if particle.x > self.box_size:
-            particle.x = self.box_size
+        if particle.x > 1:
+            particle.x = 1
             particle.vx *= -1
         if particle.y < 0:
             particle.y = 0
             particle.vy *= -1
-        if particle.y > self.box_size:
-            particle.y = self.box_size
+        if particle.y > 1:
+            particle.y = 1
             particle.vy *= -1
 
         for other_particle in self.particles:
             if particle == other_particle:
                 continue
-            elif np.sqrt((particle.x - other_particle.x)**2 + (particle.y - other_particle.y)**2) <= 0.02:
+            elif np.sqrt((particle.x - other_particle.x)**2 + (particle.y - other_particle.y)**2) <= self.rad:
                 if particle.is_infected:
-                    other_particle.infect()
+                    particle.infect(other_particle)
 
     def move(self, dt: float) -> None:
         """
@@ -164,7 +166,7 @@ class Simulation:
                 num_infections += 1
         self.infections = num_infections
 
-    def run(self, dt: float) -> Tuple[List[float], List[float], List[float], List[float], int]:
+    def run(self, dt: float) -> Tuple[List[float], List[float], List[float], List[float], int, float]:
         """
         Runs simulation until no particles are infected
 
@@ -184,6 +186,7 @@ class Simulation:
         x_inf = []
         y_inf = []
         n_rec = []
+        r0 = 0
 
         while self.infections > 0:
             x_healthy.append([])
@@ -203,10 +206,17 @@ class Simulation:
                     y_healthy[-1].append(particle.y)
             n_rec.append(recovered)
 
-        return x_healthy, y_healthy, x_inf, y_inf, n_rec
+        infecting_particles = 0
+        for particle in self.particles:
+            if particle.infections > 0:
+                r0 += particle.infections
+                infecting_particles += 1
+        r0 /= infecting_particles
+
+        return x_healthy, y_healthy, x_inf, y_inf, n_rec, r0
 
 
-def show_simulation(x_healthy: List[float], y_healthy: List[float], x_inf: List[float], y_inf: List[float], n_rec: List[int], box_size: float):
+def show_simulation(x_healthy: List[float], y_healthy: List[float], x_inf: List[float], y_inf: List[float], n_rec: List[int]):
     """
     Shows an animated simulation of particles until all particles are healthy. Opens 
     matplotlib window to show simulation and then plots the number of infected and 
@@ -219,14 +229,13 @@ def show_simulation(x_healthy: List[float], y_healthy: List[float], x_inf: List[
         x_inf (List[[float]): x-coordinates of infected particles at each timestep
         y_inf (List[float]): y-coordinates of infected particles at each timestep
         n_rec (List[int]): number of recovered particles at each timestep
-        box_size (float): length of side of simulation box
     """
 
     plt.style.use('./test.mplstyle')
     fig = plt.figure(figsize=(5,5))
     ax = fig.add_subplot(111)
-    ax.set_xlim(-0.01*box_size, 1.01*box_size)
-    ax.set_ylim(-0.01*box_size, 1.01*box_size)
+    ax.set_xlim(-0.01, 1.01)
+    ax.set_ylim(-0.01, 1.01)
     ax.set_xticks([])
     ax.set_yticks([])
 
@@ -255,7 +264,9 @@ def show_simulation(x_healthy: List[float], y_healthy: List[float], x_inf: List[
 
 
 if __name__ == "__main__":
-    sim = Simulation(200, 5, 1, 0.1, 50)
-    show_simulation(*sim.run(0.5), 1)
+    sim = Simulation(100, 5, 0.01, 1, 50)
+    x_healthy, y_healthy, x_inf, y_inf, n_rec, r0 = sim.run(0.1)
+    show_simulation(x_healthy, y_healthy, x_inf, y_inf, n_rec)
+    print(r0)
     
     
